adf136f2fdbcce061fc7dba7965ef445305dd955
codedit334 <oussama7elabdioui@gmail.com>
2023-09-13 00:20:41 +0100

    Implement localstorage functionality

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,8 @@ type Task = {
 const list = document.querySelector<HTMLUListElement>('#list');
 const form = document.getElementById('new-task-form') as HTMLFormElement || null;
 const input = document.querySelector<HTMLInputElement>('#new-task-title');
-const tasks: Task[] = []
+const tasks: Task[] = loadTasks()
+tasks.forEach(addListItem)
 
 form?.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -25,6 +26,9 @@ form?.addEventListener('submit', (e) => {
     completed: false,
     createdAt: new Date()
   }
+
+  tasks.push(newTask)
+  saveTasks()
   addListItem(newTask)
   input.value = "";
 })
@@ -33,9 +37,23 @@ function addListItem(task: Task){
   const item = document.createElement('li');
   const label = document.createElement('label');
   const checkbox = document.createElement('input');
+  checkbox.addEventListener('change', () => {
+    task.completed = checkbox.checked
+    saveTasks()
+  })
   checkbox.type = "checkbox";
   checkbox.checked = task.completed;
   label.append(checkbox, task.title);
   item.append(label);
   list?.append(item);
+}
+
+function saveTasks() {
+  localStorage.setItem('TASKS', JSON.stringify(tasks));
+}
+
+function loadTasks(): Task[] {
+  const taskJSON = localStorage.getItem('TASKS');
+  if(!taskJSON) return [];
+  return JSON.parse(taskJSON);
 }
