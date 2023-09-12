98b71dcfc70472bb443ab593ff5666e4db478071
codedit334 <oussama7elabdioui@gmail.com>
2023-09-13 00:12:55 +0100

    Create tasks arr with type Task

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ type Task = {
 const list = document.querySelector<HTMLUListElement>('#list');
 const form = document.getElementById('new-task-form') as HTMLFormElement || null;
 const input = document.querySelector<HTMLInputElement>('#new-task-title');
-
+const tasks: Task[] = []
 
 form?.addEventListener('submit', (e) => {
   e.preventDefault();
